c8fc091a2f174e7cf96944bfa62cc08b3bea73cb
Mahmoud-Barmawi <mahmoudbarmawi926@gmail.com>
2024-12-17 10:27:08 +0200

    create routing

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -4,25 +4,23 @@ import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
     return (
-        <aside>
-            <div id="upper-div">
-                <h1>Dashboard</h1>
-                <nav>
-                    <Link to="/" class="nav-link" >Overview</Link>
-                    <Link to="/villageManagment" class="nav-link" >Village Managment</Link>
-                    <Link to="/chat" class="nav-link" >Chat</Link>
-                    <Link to="/gallery" class="nav-link" >Gallery</Link>
+  <aside>
+  <div id="upper-div">
+    <h1>Dashboard</h1>
+    <nav>
+      <Link  to="/" className="nav-link">Overview</Link>
+      <Link   to="/villageManagment" className="nav-link" >Village Managment</Link>
+      <Link  to="/chat" className="nav-link">Chat</Link>
+      <Link  to="/gallery" className="nav-link">Gallery</Link>
+    </nav>
+  </div>
+  <div id="lower-div">
+    <div className="avatar" />
+    <h4>Admin Name</h4>
+    <a href="pages/login.html">Logout</a>
+  </div>
+</aside>
 
-                </nav>
-            </div>
-
-            <div id="lower-div">
-                <div class="avatar"></div>
-                <h4>Admin Name</h4>
-                <a href="pages/login.html">Logout</a>
-            </div>
-
-        </aside >
     );
 }
 
--- src/Components/Overview/Overview.jsx
+++ src/Components/Overview/Overview.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import  './Overview.css'
 
 const Overview = () => {
     return (
-        <div>
-            Overview
-        </div>
+        < >
+          <main>
+          <h1>Overview</h1> 
+          </main> 
+        </>
     );
 }
 
